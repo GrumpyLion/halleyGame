8b0b67f0d23b6b9f1d1ba98db68a6f9f84846f58
Lion Schitik <schitiklion@gmail.com>
2025-03-20 01:39:35 +0100

    Added test scene loading with a sprite entity and camera

--- src/game_stage.cpp
+++ src/game_stage.cpp
@@ -2,12 +2,23 @@
 
 void GameStage::init()
 {
+    world = createWorld("stages/game_world");
+    auto factory = EntityFactory(*world, getResources());
+    factory.createScene(getResource<Scene>("hello_world"), true);
+    factory.createEntity("camera");
 }
 
 void GameStage::onVariableUpdate(Time t)
 {
+    world->step(TimeLine::VariableUpdate, t);
+}
+
+void GameStage::onFixedUpdate(Time t)
+{
+    world->step(TimeLine::FixedUpdate, t);
 }
 
 void GameStage::onRender(RenderContext& rc) const
 {
+    world->render(rc);
 }
--- src/game_stage.h
+++ src/game_stage.h
@@ -5,8 +5,12 @@ using namespace Halley;
 
 class GameStage : public EntityStage {
 public:
-	void init() override;
+    void init() override;
 
-	void onVariableUpdate(Time) override;
-	void onRender(RenderContext&) const override;
+    void onVariableUpdate(Time t) override;
+    void onFixedUpdate(Time t) override;
+    void onRender(RenderContext&) const override;
+
+private:
+    std::unique_ptr<World> world;
 };
